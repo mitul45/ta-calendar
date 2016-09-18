b973e2375fa6894416f5841668f7a860de03bc98
Mitul Shah <45.mitul@gmail.com>
2016-09-18 21:32:22 +0530

    minor content fixes, tab now has a title :P

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 var Header = React.createClass({
   render() {
     return (
-      <h2 className='header'> ta - calender </h2>
+      <h2 className='header'> ta – calendar </h2>
     );
   }
 })
