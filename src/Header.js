2ff1157ba80e454f0219faa76c077439ec076216
Mitul Shah <45.mitul@gmail.com>
2016-09-26 22:42:40 +0530

    logo fix

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ var Header = React.createClass({
   render() {
     return (
       <div className='header'>
-        <span className='header__logo'> <img src={logo}/> </span>
+        <span className='header__logo'> <img src='https://mitul45.github.io/ta-calendar/0ef034683e41c42f958c3ea0cd3d1b47.svg'/> </span>
         <h2 className='header__text'> ta – calendar </h2>
       </div>
     );
